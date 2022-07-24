d98d45e265f91da3ad0e8ed47302845157929883
alexjiang903 <alex.jiang903@gmail.com>
2022-07-23 20:52:00 -0600

    Added caesar cipher
    epic

---
+++ caesar-cipher-4-start/main.py
@@ -0,0 +1,34 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  shift_amount %= 26
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+import art
+print(art.logo)
+
+stop_program = False
+while stop_program == False:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+  go_again = input("Type 'yes' if you want to go again and 'no' if you want to quit\n")
+  if go_again == 'yes':
+    stop_program = False
+  else:
+    stop_program = True
+print('Goodbye')
+
+
